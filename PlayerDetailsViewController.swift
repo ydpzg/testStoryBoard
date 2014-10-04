379d4fa52ca54b945365bc927de02b3eaabc9f18
zhku_ydp <yudapei@gmail.com>
2014-10-04 21:46:48 +0800

    has more a navi for tableview

--- PlayerDetailsViewController.swift
+++ PlayerDetailsViewController.swift
@@ -1,39 +1,41 @@
 //
-//  TestUITableViewController.swift
+//  PlayerDetailsViewController.swift
 //  testStoryboard
 //
-//  Created by pcbeta on 14-10-3.
+//  Created by pcbeta on 14-10-4.
 //  Copyright (c) 2014年 hgsoft. All rights reserved.
 //
 
 import UIKit
 
-class TestUITableViewController: UITableViewController, PlayerDetailsDelegate {
+protocol PlayerDetailsDelegate {
+    func cancel()
+    func done()
+}
+
+
+class PlayerDetailsViewController: UITableViewController {
+    
     var delegate:PlayerDetailsDelegate?
     
-    var data : NSMutableArray = NSMutableArray()
-    
-    func done() {
-        println("done")
-
+    @IBAction func cancel(sender: AnyObject) {
+        self.delegate?.cancel()
+        println("cancel2")
     }
-    func cancel() {
-        println("cancel")
+    
+    @IBAction func done(sender: AnyObject) {
+        self.delegate?.done()
+        println("done2")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        data.addObject("123")
-        data.addObject("456")
-        data.addObject("789")
+
         // Uncomment the following line to preserve selection between presentations
-        //self.clearsSelectionOnViewWillAppear = false
+        // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        //self.navigationItem.rightBarButtonItem = self.editButtonItem()
-        
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,49 +48,44 @@ class TestUITableViewController: UITableViewController, PlayerDetailsDelegate {
     override func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 1
+        return 0
     }
 
     override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return data.count
+        return 0
     }
 
-    
+    /*
     override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
-        let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath) as TestTableViewCell
-        cell.title.text = data[indexPath.row] as? String
-        
-        
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
+
         // Configure the cell...
 
         return cell
     }
-    
-    
+    */
+
+    /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView!, canEditRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
         // Return NO if you do not want the specified item to be editable.
         return true
     }
-    
+    */
 
-    
+    /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
-        /*
         if editingStyle == .Delete {
             // Delete the row from the data source
-            data.removeObjectAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-            
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        } 
-        */
+        }    
     }
-    
+    */
 
     /*
     // Override to support rearranging the table view.
@@ -97,13 +94,13 @@ class TestUITableViewController: UITableViewController, PlayerDetailsDelegate {
     }
     */
 
-    
+    /*
     // Override to support conditional rearranging of the table view.
     override func tableView(tableView: UITableView!, canMoveRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
         // Return NO if you do not want the item to be re-orderable.
         return true
     }
-    
+    */
 
     
     // MARK: - Navigation
@@ -112,14 +109,6 @@ class TestUITableViewController: UITableViewController, PlayerDetailsDelegate {
     override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
         // Get the new view controller using [segue destinationViewController].
         // Pass the selected object to the new view controller.
-        
-        if segue.identifier == "AddPlayer" {
-        
-            let tableController = segue.destinationViewController as? UINavigationController
-            let playerDetailsViewController = tableController?.viewControllers[0] as? PlayerDetailsViewController
-            playerDetailsViewController?.delegate = self
-        }
-   
     }
     
 
